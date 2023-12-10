8930a66bd3f708835bcf63b19ee58b4d513a364d
Suyog-Bauskar <suyogbauskar2004@gmail.com>
2023-12-10 08:21:54 +0530

    data is displayed now

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import csv
-file = open("movies.csv", 'r')
-csvFile = csv.reader(file)
-for lines in csvFile:
-    print("Rating: ", lines[4])
+import pandas as pd
+from tabulate import tabulate
+
+df = pd.read_csv("movies.csv", index_col=False, header=0)
+pd.set_option('display.max_columns', None)
+pd.set_option('display.max_rows', None)
+pd.set_option('display.width', None)
+
+print(tabulate(df, showindex=False, headers=df.columns))
